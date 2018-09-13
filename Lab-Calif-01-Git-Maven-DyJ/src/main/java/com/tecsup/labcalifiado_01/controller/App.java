d23b566ebfc5c70ede5f8ec595b4eeec672efd43
DavidGarciaGiron <david.garcia@tecsup.edu.pe>
2018-09-13 11:01:31 -0500

    First Commint

---
+++ Lab-Calif-01-Git-Maven-DyJ/src/main/java/com/tecsup/labcalifiado_01/controller/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.labcalifiado_01.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
